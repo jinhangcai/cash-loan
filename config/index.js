15e5a1993d62bc34f9e3d0a764c24412ff715e9c
654870345@qq.com <caijinhang1993>
2019-07-18 16:01:15 +0800

    东坡肉

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/m.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'qpp_static',
+    assetsSubDirectory: 'xqj_static',
     assetsPublicPath: './',
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
@@ -36,7 +36,7 @@ module.exports = {
     autoOpenBrowser: true,
     proxyTable: {
       '/web': {
-        target: 'https://qpp.nn1n.com',
+        target: 'https://xqj.nn1n.com',
         changeOrigin: true,
         pathRewrite: {
           '^/web': '/'
--- src/pages/simple/landpage-third/index.vue
+++ src/pages/simple/landpage-third/index.vue
@@ -300,9 +300,9 @@
       iosDownload () {
         switch (this.app) {
           case 'ddbt':
-            const url = 'https://qianpp.oss-cn-hangzhou.aliyuncs.com/manifest.plist'
+            const url = 'https://xiaoqingjiao.oss-cn-hangzhou.aliyuncs.com/manifest.plist'
             //   window.location.href = `itms-services://?action=download-manifest&url=${url}`
-            window.location.href = `itms-services://?action=download-manifest&url=https://qianpp.oss-cn-hangzhou.aliyuncs.com/manifest.plist`
+            window.location.href = `itms-services://?action=download-manifest&url=https://xiaoqingjiao.oss-cn-hangzhou.aliyuncs.com/manifest.plist`
             break
           default:
             break
@@ -314,7 +314,7 @@
         if(this.sendCodeTime==0){
           switch (this.app) {
             case 'ddbt':
-              window.self.location = 'https://qianpp.oss-cn-hangzhou.aliyuncs.com/embedded.mobileprovision'
+              window.self.location = 'https://xiaoqingjiao.oss-cn-hangzhou.aliyuncs.com/embedded.mobileprovision'
               break
             default:
               break
