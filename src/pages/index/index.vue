adfe1eaeed64f0c1dcc718a6ebc1c9bc4f778ec0
wangfei <wangf_m77@163.com>
2019-07-24 00:01:28 +0800

    111

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -24,7 +24,7 @@
                 <!--　零钱盒子-->
                 <!--&lt;!&ndash;<a @click="$router.push('/set')"></a>&ndash;&gt;-->
               <!--</div>-->
-              <div class="titles">
+              <div class="titles" :style="`margin-top:${native.statusBarHeight + 20}px`">
                 温馨提示:请认准APP中官方客服,其他(百度)搜索到的客服都是骗子
               </div>
               <img  src="./index/indexbgs.png" width="100%">
