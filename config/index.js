be92ea44abca74d13df63f826fdf8347ec9e310c
654870345@qq.com <caijinhang1993>
2019-07-25 09:36:25 +0800

    花花无忧

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/m.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'dpr_static',
+    assetsSubDirectory: 'hhwy_static',
     assetsPublicPath: './',
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
@@ -36,7 +36,7 @@ module.exports = {
     autoOpenBrowser: true,
     proxyTable: {
       '/web': {
-        target: 'https://dpr.huitianservice.top',
+        target: 'https://hhwy.baotoulongtian.cn',
         changeOrigin: true,
         pathRewrite: {
           '^/web': '/'
